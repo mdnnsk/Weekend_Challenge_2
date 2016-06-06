5eea20af93575586bf2f5f0c5ecbae70a3e6eec1
ngorbunov <nick_g@outlook.com>
2016-06-06 11:40:31 -0500

    added counter

--- app.js
+++ app.js
@@ -7,18 +7,13 @@ var nextButton = document.createElement('button');
 $(document).ready(function() {
 
   $('.header').append('<h1>Nu Students</h1>');
+
   $('.prev').append( prevButton );
   $('.next').append( nextButton );
   prevButton.textContent = 'Previous Student';
   nextButton.textContent = 'Next Student';
   prevButton.id = 'previous';
   nextButton.id = 'next';
-
-
-  $.getJSON('https://raw.githubusercontent.com/devjanaprime/2.4-jQueryAjaxJSON/master/students.json', function(data){
-       console.log( 'in getJSON' );
-       console.log( data );
-     }); // end getJSON button click
 
   $.ajax({
        url: 'https://raw.githubusercontent.com/devjanaprime/2.4-jQueryAjaxJSON/master/students.json',
@@ -29,7 +24,8 @@ $(document).ready(function() {
           cohort = data;
           dataString = "<p><h2>First name: " + cohort.students[i].first_name + "</h2></p>" + "<p><h2>Last name: " + cohort.students[i].last_name + "</h2></p>" + "<p><h2>From: " + cohort.students[i].city + "</h2></p>"+ "<p><h3>Shoutout: " + cohort.students[i].shoutout + "</h3></p>";
           $('.information').append( dataString );
-          console.log(cohort.students.length);
+          $('.count').append('Showing ' + (i+1) + ' of ' + cohort.students.length);
+          // console.log(cohort.students.length);
          }, // end success
        statusCode: {
           404: function(){
@@ -38,13 +34,16 @@ $(document).ready(function() {
          } // end statusCode
        }); // end ajax  object
 
+
+
   $('#previous').on('click', function(){
     i--;
     if (i<0) {
       i = cohort.students.length - 1;
     }
-    // $('.information').html( dataString );
+    // $('.information').html( dataString ); - does not work!!!
     $('.information').html( "<p><h2>First name: " + cohort.students[i].first_name + "</h2></p>" + "<p><h2>Last name: " + cohort.students[i].last_name + "</h2></p>" + "<p><h2>From: " + cohort.students[i].city + "</h2></p>"+ "<p><h3>Shoutout: " + cohort.students[i].shoutout + "</h3></p>" );
+    $('.count').html('Showing ' + (i+1) + ' of ' + cohort.students.length);
   });
 
   $('#next').on('click', function(){
@@ -54,6 +53,7 @@ $(document).ready(function() {
     }
     // $('.information').html( dataString );
     $('.information').html( "<p><h2>First name: " + cohort.students[i].first_name + "</h2></p>" + "<p><h2>Last name: " + cohort.students[i].last_name + "</h2></p>" + "<p><h2>From: " + cohort.students[i].city + "</h2></p>"+ "<p><h3>Shoutout: " + cohort.students[i].shoutout + "</h3></p>" );
+    $('.count').html('Showing ' + (i+1) + ' of ' + cohort.students.length);
   });
 
 });
